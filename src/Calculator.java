3d1147261e0a48309200cd2461b4f159bd2532f1
myung <myung@192.168.1.2>
2017-09-12 16:39:22 +0900

    2017년 9월 12일

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
 		
-		System.out.println();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("첫 번째 입력 값: ");
+		int first = scanner.nextInt();
+		System.out.println(first);
+		
+		System.out.println("두 번째 입력 값: ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		
+		System.out.println("덧셈: " + (first + second));
+		System.out.println("뺄셈: " + (first - second ));
+		System.out.println("곱셈: " + (first * second));
+		System.out.println("나눗셈: " + (first / second));
 	}
 }
